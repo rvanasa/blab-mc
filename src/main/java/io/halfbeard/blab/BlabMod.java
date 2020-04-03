a0ed374d1a3ceac13c438638932612a1c808e5b3
Ryan Vandersmith <ryanvandersmith@gmail.com>
2020-04-02 20:59:19 -0600

    TTS progress

--- src/main/java/io/halfbeard/blab/BlabMod.java
+++ src/main/java/io/halfbeard/blab/BlabMod.java
@@ -3,38 +3,28 @@ package io.halfbeard.blab;
 import marytts.LocalMaryInterface;
 import marytts.MaryInterface;
 import marytts.exceptions.MaryConfigurationException;
-import marytts.exceptions.SynthesisException;
-import marytts.modules.synthesis.Voice;
 import marytts.util.data.audio.AudioPlayer;
 import net.minecraft.block.Block;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.util.text.event.ClickEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.event.entity.EntityEvent;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import sun.audio.AudioStream;
 
 import javax.sound.sampled.AudioInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Objects;
 import java.util.Random;
 
 import static io.halfbeard.blab.BlabMod.BLAB_MOD;
-import static io.halfbeard.blab.BlabUtils.randomDialogData;
+import static io.halfbeard.blab.BlabMod.EventListeners.*;
 
 @Mod(BLAB_MOD)
 public class BlabMod {
@@ -46,6 +36,7 @@ public class BlabMod {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
         MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.addListener(EventListeners::livingHurt);///////
     }
 
     private void setup(FMLCommonSetupEvent event) {
@@ -76,54 +67,44 @@ public class BlabMod {
         private static final Random RANDOM = new Random();
         private static final MaryInterface MARY;
         private static final String[] VOICES;
+        private static AudioPlayer prevPlayer;
 
         static {
             try {
                 MARY = new LocalMaryInterface();
                 VOICES = MARY.getAvailableVoices().toArray(new String[0]);
+
+                MARY.setVoice(VOICES[0]);
             }
             catch(MaryConfigurationException e) {
                 throw new RuntimeException(e);
             }
         }
 
-        @SubscribeEvent
+        // TODO refactor
         public static void livingHurt(LivingHurtEvent event) {
-
-            LOGGER.error("$#^$%&$%&$%^&^%*$%^*%$*%$*%^*%^*$%*%$" + event.getSource() + " " + event.getEntityLiving());
+            if(event.getEntityLiving().getHealth() <= event.getAmount()) {
+                return;
+            }
 
             if(event.getEntityLiving() instanceof VillagerEntity) {
-                VillagerEntity villager = (VillagerEntity) event.getEntityLiving();
+//                VillagerEntity villager = (VillagerEntity) event.getEntityLiving();
 
-                MARY.setVoice(VOICES[RANDOM.nextInt(VOICES.length)]);
+                if(prevPlayer != null) {
+                    prevPlayer.cancel();
+                }
 
                 DialogData data = BlabUtils.randomDialogData("hurt");
                 try(AudioInputStream stream = MARY.generateAudio(data.text)) {
                     AudioPlayer player = new AudioPlayer();
                     player.setAudio(stream);
                     player.start();
+                    prevPlayer = player;
                 }
                 catch(Exception e) {
                     e.printStackTrace();
                 }
             }
-
-            event.getEntityLiving().setHealth(0);
-        }
-
-        @SubscribeEvent
-        public static void temp2(PlayerInteractEvent event) {
-                MARY.setVoice(VOICES[RANDOM.nextInt(VOICES.length)]);
-
-                DialogData data = BlabUtils.randomDialogData("hurt");
-                try(AudioInputStream stream = MARY.generateAudio(data.text)) {
-                    AudioPlayer player = new AudioPlayer();
-                    player.setAudio(stream);
-                    player.start();
-                }
-                catch(Exception e) {
-                    e.printStackTrace();
-                }
         }
 
         @SubscribeEvent
